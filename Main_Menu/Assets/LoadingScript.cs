e88aeb6e4cd8601449097aa315e1c2088c045cf9
Igor_br <contact@igorbrai.me>
2018-03-14 18:04:04 +0000

    The main menu

---
+++ Main_Menu/Assets/LoadingScript.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LoadingScript : MonoBehaviour {
+    public Sprite image;
+
+
+    public void LoadLvl(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));        
+
+    }
+
+    private void StartCoroutine(IEnumerable enumerable)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerable LoadAsync (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while ( !operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+
+           Color c = GetComponent<SpriteRenderer>().color;
+            c.a = progress * 10;
+            GetComponent<SpriteRenderer>().color = c;
+
+
+        }
+
+
+
+
+        yield return null;
+
+    }
+    
+
+    
+}
---
+++ Main_Menu/Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class OptionMenu : MonoBehaviour {
+
+    public AudioMixer audiomix;
+    public Dropdown resolutionDropdown;
+
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int curentRes = 0;
+        for ( int i = 0; i < resolutions.Length; i ++ )
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+
+            if ((resolutions[i].width  == Screen.currentResolution.width) &&
+                (resolutions[i].height == Screen.currentResolution.height))
+            {
+                curentRes = i;
+            }
+        }
+         
+
+
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = curentRes;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+
+    public void setResolution(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audiomix.SetFloat("volume", volume);
+    }
+
+    public void ChangeQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void FullScreen(bool full)
+    {
+        Screen.fullScreen = full;
+    }
+}
