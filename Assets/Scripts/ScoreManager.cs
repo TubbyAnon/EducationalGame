a8f64f8b5ba0036bf8f567964c227ffde355b9bb
ayaa11 <36631019+ayaa11@users.noreply.github.com>
2018-05-01 23:06:57 +0100

    Fixed GUI

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+	public static int score;    // The player's score
+	public Text text;          // Reference to the Text component
+
+	void Start(){
+		text.text = "";
+	}
+	void Awake ()
+	{
+		text = GetComponent <Text> ();  // Set up the reference
+		score = 0;  // Reseting the score
+	}
+	void Update ()
+	{
+		// Set the displayed text to be the word "Score" followed by the score value
+		text.text = "Score: " + score;
+	}
+}
