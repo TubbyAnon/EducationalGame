0cafa056970a86f7a032ff05e15f74fcfc8b028a
unknown <archie.burstow@outlook.com>
2018-05-04 00:28:50 +0100

    fixed some issues, have linked menu and main scene

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,8 @@ public class Spawner : MonoBehaviour {
 	public float delay = 2.0f;
 	public bool active = true;
 	public Vector2 delayRange = new Vector2(1, 2);
+    public int xPos;
+    public int sW = Screen.width;
 
 	// Use this for initialization
 	void Start () {
@@ -20,8 +22,13 @@ public class Spawner : MonoBehaviour {
 
 		if (active) {
 			var newTransform = transform;
+            System.Random rnd = new System.Random();
+            xPos = rnd.Next(0, sW);
+            Vector3 newPos = transform.position;
+            newPos.x = xPos;
+            transform.position = newPos; 
 
-			GameObjectUtil.Instantiate(prefabs[Random.Range(0, prefabs.Length)], newTransform.position);
+            GameObjectUtil.Instantiate(prefabs[Random.Range(0, prefabs.Length)], newPos);
 			ResetDelay();
 		}
 
--- Assets/Scripts/moveplayer.cs
+++ Assets/Scripts/moveplayer.cs
@@ -2,55 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveplayer : MonoBehaviour {
+public class moveplayer : MonoBehaviour
+{
 
     public GameObject bullet;
 
     void Update()
-
     {
-
         if (Input.GetKeyDown(KeyCode.Space))
-
         {
-
             GameObject b = (GameObject)(Instantiate(bullet, transform.position + transform.up * 1.5f, Quaternion.identity));
-
             b.GetComponent<Rigidbody2D>().AddForce(transform.up * 1000);
-
         }
-
         if (Input.GetKey(KeyCode.LeftArrow))
-
         {
-
-            gameObject.transform.Translate(Vector3.left * 0.1f);
-
+                gameObject.transform.Translate(Vector3.left * 0.1f);
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
-
         {
-
-            gameObject.transform.Translate(Vector3.right * 0.1f);
-
+                gameObject.transform.Translate(Vector3.right * 0.1f);
         }
+        //if (Input.GetKey(KeyCode.UpArrow))
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        //{
 
-        {
+        //    gameObject.transform.Translate(Vector3.up * 0.1f);
 
-            gameObject.transform.Translate(Vector3.up * 0.1f);
+        //}
 
-        }
+        //if (Input.GetKey(KeyCode.DownArrow))
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        //{
 
-        {
+        //    gameObject.transform.Translate(Vector3.down * 0.1f);
 
-            gameObject.transform.Translate(Vector3.down * 0.1f);
-
-        }
-
+        //}
     }
 }
