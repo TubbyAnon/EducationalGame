e1f5be498327e5199487e1558d1b39497da01dd4
Benjamin Butler <benjaminbutler75@hotmail.com>
2018-05-04 18:11:19 +0100

    new player and shooting

---
+++ Assets/Scripts/spaceshipscript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceshipscript : MonoBehaviour
+{
+    public GameObject PlayerBulletGo;
+    public GameObject BulletPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            GameObject bullet = (GameObject)Instantiate(PlayerBulletGo);
+            bullet.transform.position = BulletPosition.transform.position;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(Vector3.left * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(Vector3.right * 0.1f);
+        }
+    }
+}
+
