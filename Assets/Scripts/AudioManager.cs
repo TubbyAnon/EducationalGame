de9c289d2408643a6cff70bc2014cbebc862d041
ashwaq <a.alharbi2@uni.brighton.com>
2018-05-25 02:50:53 +0100

    music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    // the audio clip
+    public AudioClip MusicClip;
+
+    // the component that Unity uses to play your clip
+    public AudioSource MusicSource;
+
+	void Start () {
+        MusicSource.clip = MusicClip;
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            MusicSource.Play();	
+	}
+}
